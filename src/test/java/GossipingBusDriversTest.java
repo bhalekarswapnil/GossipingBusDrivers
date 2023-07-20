b4d5d8dc69d3f84620d666d12a55dc16badcee17
bhalekarswapnil <85471791+bhalekarswapnil@users.noreply.github.com>
2023-07-20 18:01:40 +0530

    Update GossipingBusDriversTest.java

--- src/test/java/GossipingBusDriversTest.java
+++ src/test/java/GossipingBusDriversTest.java
@@ -11,7 +11,7 @@ public class GossipingBusDriversTest {
                 {2, 1, 2},
                 {5, 2, 8}
         };
-        int expectedOutput = -1; // never
+        int expectedOutput = -1; // never gossips
         assertEquals(expectedOutput, GossipingBusDrivers.getNumberStopsForSharingAllGosips(driverRoutes));
     }
 
@@ -34,7 +34,7 @@ public class GossipingBusDriversTest {
                 {3,2,3,1},
                 {4, 2,3,4,5 },
         };
-        int expectedOutput = 5; // All drivers meet at stop 12, so all gossips in 12 stops
+        int expectedOutput = 5; 
         assertEquals(expectedOutput, GossipingBusDrivers.getNumberStopsForSharingAllGosips(driverRoutes));
     }
 }
